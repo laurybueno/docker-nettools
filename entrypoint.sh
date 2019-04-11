71d9eec3ae3edc2fd231d2af6e22fe1a6207da81
Laury Bueno <laury@hacklab.com.br>
2019-04-11 11:42:30 -0300

    Revert "Fix password setting when creating a user"
    This reverts commit abaa396ab40742ca40892dc905c1cd4821d0ee1b.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,55 +3,26 @@
 # Set user Password
 if [[ ! -z $ROOT_PASSWORD ]]; then
 
-  $(echo -e "$ROOT_PASSWORD\n$ROOT_PASSWORD" | passwd root &> /dev/null)
+  $(echo -e "$ROOT_PASSWORD\n$ROOT_PASSWORD" | passwd root )
 
 fi
 
-# Enable Apache www-data user shell
-if [[ $USER_APACHE == True ]]; then
-  
-  $(mkdir -p /var/www/html)
-  $(usermod -d /var/www/html --shell /bin/bash www-data)
-  
-  if [[ ! -z $APACHE_PASSWORD ]]; then
-  $(echo -e "$APACHE_PASSWORD\n$APACHE_PASSWORD" | passwd  www-data  &> /dev/null)
-  fi 
-fi
 
 # Check if username and password are set, if is check for alternative user folder.
 if [[ ! -z $USER_NAME ]] && [[ ! -z  $USER_PASSWORD ]]; then
   
-  # check User ID, Group ID and Folder are Set
-
-  if [[ ! -z $USER_ID ]] && [[ ! -z  $GROUP_ID ]] && [[ ! -z $USER_FOLDER ]]; then
- echo "user folder line +UID+GID"
-    $(mkdir -p $USER_FOLDER)
-    $(groupadd $GROUP_ID)
-    $(useradd $USER_NAME -p $USER_PASSWORD -u $USER_ID -d $USER_FOLDER --shell /bin/bash)
-    $(echo -e "$USER_PASSWORD\n$USER_PASSWORD" | passwd $USER_NAME &> /dev/null)
-    $(groupmod -g $GROUP_ID $USER_NAME)
-
-  # check User ID  and Group ID are Set
-  elif [[ ! -z $USER_ID ]] && [[ ! -z  $GROUP_ID ]]; then
-	  
-    $(groupadd $GROUP_ID)
-    $(useradd $USER_NAME -m -p $USER_PASSWORD -u $USER_ID --shell /bin/bash)
-    $(echo -e "$USER_PASSWORD\n$USER_PASSWORD" | passwd $USER_NAME &> /dev/null)
-    $(groupmod -g $GROUP_ID $USER_NAME)
-
-  # check if only alternative Folder are Set.
-  elif [[ ! -z $USER_FOLDER ]]; then
+  if [[ ! -z $USER_FOLDER ]]; then
 
     $(mkdir -p $USER_FOLDER)
     $(useradd $USER_NAME -d $USER_FOLDER --shell /bin/bash)
-    $(echo -e "$USER_PASSWORD\n$USER_PASSWORD" | passwd $USER_NAME &> /dev/null)
+    $(echo -e "$USER_PASSWORD\n$USER_PASSWORD" | passwd $USER_NAME )
 
   else
-    $(useradd $USER_NAME -m --shell /bin/bash)
-    $(echo -e "$USER_PASSWORD\n$USER_PASSWORD" | passwd $USER_NAME &> /dev/null)
+
+    $(useradd $USER_NAME -d $USER_FOLDER --shell /bin/bash)
+    $(echo -e "$USER_PASSWORD\n$USER_PASSWORD" | passwd $USER_NAME )
 
   fi
-
 fi  
 
 exec "$@" 
